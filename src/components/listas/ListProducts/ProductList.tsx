69c1fdfa32be315b6101fd36a81a2227c6ca6021
Maximo <maxife924@gmail.com>
2024-11-05 09:26:18 -0300

    Lista productos

---
+++ src/components/listas/ListProducts/ProductList.tsx
@@ -0,0 +1,58 @@
+import  { FC, useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+import { IProductos } from "../../../types/dtos/productos/IProductos";
+
+interface IListaProductsProps {
+	productos: IProductos[];
+}
+
+const ProductList: FC<IListaProductsProps> = ({productos}) => {
+
+    const [listaProductos, setListaProductos] = useState<IProductos[]>([]);
+
+	useEffect(() => {
+		setListaProductos(productos);
+	}, [productos]);
+
+  return (
+    <div>
+      <div>
+    
+      {listaProductos.length > 0 ? (
+    <div>
+        <Table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Precio</th>
+                    <th>Descripción</th>
+                    <th>Habilitado</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listaProductos.map((producto, index) => (
+                    <tr key={index}>
+                        <td>{index + 1}</td> {/* Número de fila */}
+                        <td>{producto.precioVenta}</td>
+                        <td>{producto.descripcion}</td>
+                        <td>{producto.habilitado ? "Sí" : "No"}</td>
+                        <td>
+                            <button>editar</button>
+                            <button>eliminar</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    </div>
+) : (
+    <div>No hay productos creados</div>
+)}
+       
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
