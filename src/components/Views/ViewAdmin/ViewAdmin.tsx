bd5ad945b342cdae03d3b6dd760f6276ff75f577
Maximo <maxife924@gmail.com>
2024-11-05 09:29:03 -0300

    ViewAdmin

--- src/components/Views/ViewAdmin/ViewAdmin.tsx
+++ src/components/Views/ViewAdmin/ViewAdmin.tsx
@@ -1,52 +1,142 @@
-import { useSelector } from "react-redux";
-import styles from "./ViewAdmin.module.css";
-import { RootState } from "../../../redux/store/store";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import React, { useState } from 'react';
+
+import React, { useEffect, useState } from "react";
+import { Container, Navbar } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
+import style from "./ViewAdmin.module.css";
+import CategorieList from "../../listas/ListCategories/CategorieList";
+import ProductList from "../../listas/ListProducts/ProductList";
+import AlergenosList from "../../listas/ListAlergenos/AlergenosList";
+import { IProductos } from "../../../types/dtos/productos/IProductos";
+import { productoService } from "../../../services/productoService";
+import {categoriaService} from "../../../services/categoriaService"
+import { alergenoService } from "../../../services/alergenoService";
+import { ICategorias } from "../../../types/dtos/categorias/ICategorias";
+import { IAlergenos } from "../../../types/dtos/alergenos/IAlergenos";
+import ModalCrearProducto from "../../modals/Producto/ModalCrearProducto/ModalCrearProducto";
+
 
 const ViewAdmin: React.FC = () => {
-  // Estado que guarda la pestaña activa
-  const [activeTab, setActiveTab] = useState<'Categorias' | 'Productos' | 'Alergenos'>('Categorias');
+  
 
+  const [productos, setProductos] = useState<IProductos[]>([]);
+  const [categorias, setCategorias] = useState<ICategorias[]>([])
+  const [alergenos, setAlergenos] = useState<IAlergenos[]>([])
+  // Estado que guarda la pestaña activa
+  const [activeTab, setActiveTab] = useState<
+    "Categorias" | "Productos" | "Alergenos"
+  >("Categorias");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+
+  const {branchName} = location.state || []
   // Función para cambiar la pestaña activa
-  const handleTabChange = (tab: 'Categorias' | 'Productos' | 'Alergenos') => {
+  const handleTabChange = (tab: "Categorias" | "Productos" | "Alergenos") => {
     setActiveTab(tab);
   };
 
-  return (
-    <div className="admin-container">
-      <header className="header">
-        <button className="back-button">←</button>
-        <h1>BENDITO RUFIÁN - LA BARRACA</h1>
-      </header>
-      <div className="admin-menu">
-        {/* Botones para cambiar de pestaña */}
-        <button
-          onClick={() => handleTabChange('Categorias')}
-          className={`menu-button ${activeTab === 'Categorias' ? 'active' : ''}`}
-        >
-          Categorias
-        </button>
-        <button
-          onClick={() => handleTabChange('Productos')}
-          className={`menu-button ${activeTab === 'Productos' ? 'active' : ''}`}
-        >
-          Productos
-        </button>
-        <button
-          onClick={() => handleTabChange('Alergenos')}
-          className={`menu-button ${activeTab === 'Alergenos' ? 'active' : ''}`}
-        >
-          Alergenos
-        </button>
-      </div>
+  const handleHomeRedirect = () => {
+    navigate("/"); //VUELVE AL HOME 
+  };
 
-      {/* Contenido de la pestaña activa */}
-      <div className="content-area">
-        {activeTab === 'Categorias' && <p>Contenido de Categorías</p>}
-        {activeTab === 'Productos' && <p>Contenido de Productos</p>}
-        {activeTab === 'Alergenos' && <p>Contenido de Alérgenos</p>}
+  const [mostrarPopUp, setMostrarPopUp] = useState<boolean>(false);
+
+	const handleOpenPopUp = () => {
+		setMostrarPopUp(true);
+	};
+
+	const handleClosePopUp = () => {
+		setMostrarPopUp(false);
+	};
+
+
+  useEffect(() => {
+    const fetch = async () => {
+        try {
+            switch (activeTab) {
+                case "Categorias":
+                    const categoriaDatos = await categoriaService.getAllCategorias();
+                    setCategorias(categoriaDatos);
+                    break;
+                case "Productos":
+                    const productoDatos = await productoService.getAllProductos();
+                    setProductos(productoDatos);
+                    break;
+                case "Alergenos":
+                    const alergenoDatos = await alergenoService.getAllAlergenos();
+                    setAlergenos(alergenoDatos);
+                    break;
+                default:
+                    break;
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    fetch();
+}, [activeTab]);
+
+
+  return (
+    <div className={style.principalContainerAdmin}>
+      <header>
+        <Navbar expand="lg">
+          <Container className={style.NavHeader}>
+            <button className={style.backButton} onClick={handleHomeRedirect}>
+              <span className="material-symbols-outlined">arrow_back</span>
+            </button>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Navbar.Text>
+                <h3>{branchName ? `Admin - ${branchName}` : 'Admin'}</h3>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+
+      <div className={style.adminContainer}>
+        <div className={style.adminMenu}>
+          {/* Botones para cambiar de pestaña */}
+          <button
+            onClick={() => handleTabChange("Categorias")}
+            className={`${style.menuButton} ${activeTab === "Categorias" ? style.active : ""}`}
+          >
+            Categorias
+          </button>
+          <button
+            onClick={() => handleTabChange("Productos")}
+            className={`${style.menuButton} ${activeTab === "Productos" ? style.active : ""}`}
+          >
+            Productos
+          </button>
+          <button
+            onClick={() => handleTabChange("Alergenos")}
+            className={`${style.menuButton} ${activeTab === "Alergenos" ? style.active : ""}`}
+          >
+            Alergenos
+          </button>
+        </div>
+        {mostrarPopUp && (
+				<>
+					<ModalCrearProducto
+						show={mostrarPopUp}
+						onHide={handleClosePopUp}
+					/>
+				</>
+			)}
+        {/* Contenido de la pestaña activa */}
+        <div className="content-area">
+          <h3>{activeTab.toUpperCase()}</h3>
+          <button onClick={handleOpenPopUp}>AGREGAR {activeTab.toUpperCase()}</button>
+
+          <div>
+            {activeTab === "Categorias" && <CategorieList categorias={categorias} />}
+            {activeTab === "Productos" && <ProductList productos={productos}/>}
+            {activeTab === "Alergenos" && <AlergenosList alergenos={alergenos}/>}
+          </div>
+        </div>
       </div>
     </div>
   );
