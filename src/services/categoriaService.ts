0eda8e66c77223f1320c794323701e93dd3303db
Maximo <maxife924@gmail.com>
2024-11-11 23:43:06 -0300

    categoria service

--- src/services/categoriaService.ts
+++ src/services/categoriaService.ts
@@ -1,16 +1,13 @@
 import axios from "axios";
 import { ICategorias } from "../types/dtos/categorias/ICategorias";
 import { ICreateCategoria } from "../types/dtos/categorias/ICreateCategoria";
-import { IProductos } from "../types/dtos/productos/IProductos";
-
-
 
 export const BASE_URL_CATEGORIAS = "http://190.221.207.224:8090/categorias";
 
 
 export const categoriaService = {
     async getCategoriaByID(id: number): Promise<ICategorias>{
-        const response = await axios.get<ICategorias>(`${BASE_URL_CATEGORIAS}`)
+        const response = await axios.get<ICategorias>(`${BASE_URL_CATEGORIAS}/${id}`)
         return response.data
     },
     async getAllCategorias():Promise<ICategorias[]>{
