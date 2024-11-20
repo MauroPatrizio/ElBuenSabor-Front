7aba0bb27bd295176bd647ca4e6528d4eadc670a
Mauro Patrizio <maurodanielpatrizio@gmail.com>
2024-11-20 19:01:31 -0300

    estilos de productos y viewAdmin

--- src/components/listas/ListProducts/ProductList.tsx
+++ src/components/listas/ListProducts/ProductList.tsx
@@ -7,147 +7,154 @@ import { productoService } from "../../../services/productoService";
 import Swal from "sweetalert2";
 
 interface IListaProductsProps {
-  productos: IProductos[];
+	productos: IProductos[];
 }
 
 const ProductList: FC<IListaProductsProps> = ({ productos }) => {
-  const [listaProductos, setListaProductos] = useState<IProductos[]>([]);
-  const [productoSeleccionado, setProductoSeleccionado] =
-    useState<IProductos | null>(null);
+	const [listaProductos, setListaProductos] = useState<IProductos[]>([]);
+	const [productoSeleccionado, setProductoSeleccionado] = useState<IProductos | null>(null);
 
-  useEffect(() => {
-    setListaProductos(productos);
-  }, [productos]);
+	useEffect(() => {
+		setListaProductos(productos);
+	}, [productos]);
 
-  const [mostrarPopUp, setMostrarPopUp] = useState<boolean>(false);
-  const [modoEdicion, setModoEdicion] = useState<boolean>(false);
+	const [mostrarPopUp, setMostrarPopUp] = useState<boolean>(false);
+	const [modoEdicion, setModoEdicion] = useState<boolean>(false);
 
-  const handleOpenPopUp = () => {
-    setMostrarPopUp(true);
-  };
+	const handleOpenPopUp = () => {
+		setMostrarPopUp(true);
+	};
 
-  const handleOpenPopUpEdicion = (productoSelec: IProductos) => {
-    setProductoSeleccionado(productoSelec);
-    setModoEdicion(true);
-  };
+	const handleOpenPopUpEdicion = (productoSelec: IProductos) => {
+		setProductoSeleccionado(productoSelec);
+		setModoEdicion(true);
+	};
 
-  const handleClosePopUp = () => {
-    setMostrarPopUp(false);
-    setModoEdicion(false);
-    setProductoSeleccionado(null);
-  };
+	const handleClosePopUp = () => {
+		setMostrarPopUp(false);
+		setModoEdicion(false);
+		setProductoSeleccionado(null);
+	};
 
-  const handleDelete = async (id: number) => {
-    const result = await Swal.fire({
-      title: "¿Estás seguro?",
-      text: "Esta acción no se puede deshacer.",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Sí, eliminar",
-      cancelButtonText: "Cancelar",
-    });
+	const handleDelete = async (id: number) => {
+		const result = await Swal.fire({
+			title: "¿Estás seguro?",
+			text: "Esta acción no se puede deshacer.",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Sí, eliminar",
+			cancelButtonText: "Cancelar",
+		});
 
-    if (result.isConfirmed) {
-      try {
-        await productoService.deleteProduct(id);
-        Swal.fire({
-          icon: "success",
-          title: "Producto eliminado con éxito",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        // Actualizar la lista local después de eliminar
-        setListaProductos((prev) =>
-          prev.filter((producto) => producto.id !== id)
-        );
-      } catch (error) {
-        console.error("Error al eliminar el producto:", error);
-        Swal.fire({
-          icon: "error",
-          title: "Error al eliminar",
-          text: "Intente nuevamente más tarde.",
-        });
-      }
-    }
-  };
+		if (result.isConfirmed) {
+			try {
+				await productoService.deleteProduct(id);
+				Swal.fire({
+					icon: "success",
+					title: "Producto eliminado con éxito",
+					showConfirmButton: false,
+					timer: 1500,
+				});
+				// Actualizar la lista local después de eliminar
+				setListaProductos((prev) => prev.filter((producto) => producto.id !== id));
+			} catch (error) {
+				console.error("Error al eliminar el producto:", error);
+				Swal.fire({
+					icon: "error",
+					title: "Error al eliminar",
+					text: "Intente nuevamente más tarde.",
+				});
+			}
+		}
+	};
 
-  return (
-    <div>
-      <h3 className="text-center">PRODUCTOS</h3>
-      <Button
-        onClick={handleOpenPopUp}
-        variant="primary"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        Agregar Producto
-      </Button>
+	return (
+		<div>
+			<h3 className="text-center">PRODUCTOS</h3>
+			<div style={{ display: "flex", justifyContent: "center", margin: "1.5rem" }}>
+				<Button
+					variant="primary"
+					onClick={handleOpenPopUp}
+				>
+					Agregar Producto
+				</Button>
+			</div>
 
-      <div>
-        {listaProductos.length > 0 ? (
-          <div>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Denominación</th>
-                  <th>Descripción</th>
-                  <th>Precio</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                {listaProductos.map((producto, index) => (
-                  <tr key={producto.id}>
-                    <td className="text-center">{index + 1}</td>{" "}
-                    {/* Número de fila */}
-                    <td className="text-center">{producto.denominacion}</td>
-                    <td className="text-center">{producto.descripcion}</td>
-                    <td className="text-center">{producto.precioVenta}</td>
-                    <td
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-evenly",
-                      }}
-                    >
-                      <Button
-                        variant="warning"
-                        onClick={() => handleOpenPopUpEdicion(producto)}
-                      >
-                        <span className="material-symbols-outlined">edit</span>
-                      </Button>
-                      <Button
-                        variant="danger"
-                        onClick={() => handleDelete(producto.id)}
-                      >
-                        <span className="material-symbols-outlined">
-                          delete
-                        </span>
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div>
-        ) : (
-          <div>No hay productos creados</div>
-        )}
-      </div>
+			<div>
+				{listaProductos.length > 0 ? (
+					<div>
+						<Table
+							striped
+							bordered
+							hover
+						>
+							<thead>
+								<tr>
+									<th className="text-center">#</th>
+									<th className="text-center">Precio</th>
+									<th className="text-center">Descripción</th>
+									<th className="text-center">Habilitado</th>
+									<th className="text-center">Acciones</th>
+								</tr>
+							</thead>
+							<tbody>
+								{listaProductos.map((producto, index) => (
+									<tr key={producto.id}>
+										<td className="text-center">{index + 1}</td>{" "}
+										{/* Número de fila */}
+										<td className="text-center">{producto.denominacion}</td>
+										<td className="text-center">{producto.descripcion}</td>
+										<td className="text-center">{producto.precioVenta}</td>
+										<td
+											style={{
+												display: "flex",
+												justifyContent: "space-evenly",
+											}}
+										>
+											<Button
+												variant="warning"
+												onClick={() => handleOpenPopUpEdicion(producto)}
+											>
+												<span className="material-symbols-outlined">
+													edit
+												</span>
+											</Button>
+											<Button
+												variant="danger"
+												onClick={() => handleDelete(producto.id)}
+											>
+												<span className="material-symbols-outlined">
+													delete
+												</span>
+											</Button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</Table>
+					</div>
+				) : (
+					<div>No hay productos creados</div>
+				)}
+			</div>
 
-      {mostrarPopUp && (
-        <ModalCrearProducto show={mostrarPopUp} onHide={handleClosePopUp} />
-      )}
-      {modoEdicion && productoSeleccionado && (
-        <ModalEditarProducto
-          show={modoEdicion}
-          onHide={handleClosePopUp}
-          producto={productoSeleccionado}
-        />
-      )}
-    </div>
-  );
+			{mostrarPopUp && (
+				<ModalCrearProducto
+					show={mostrarPopUp}
+					onHide={handleClosePopUp}
+				/>
+			)}
+			{modoEdicion && productoSeleccionado && (
+				<ModalEditarProducto
+					show={modoEdicion}
+					onHide={handleClosePopUp}
+					producto={productoSeleccionado}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default ProductList;
--- src/components/modals/Producto/ModalCrearProducto/ModalCrearProducto.tsx
+++ src/components/modals/Producto/ModalCrearProducto/ModalCrearProducto.tsx
@@ -1,161 +1,171 @@
 import React, { FC, useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 import Swal from "sweetalert2";
-import styles from "./ModalCrearProducto.module.css";
 import { productoService } from "../../../../services/productoService";
 import { ICreateProducto } from "../../../../types/dtos/productos/ICreateProducto";
 
 interface IModalCrearProductoProps {
-  show: boolean;
-  onHide: () => void;
+	show: boolean;
+	onHide: () => void;
 }
 
 export const ModalCrearProducto: FC<IModalCrearProductoProps> = ({ show, onHide }) => {
-  const [formData, setFormData] = useState<ICreateProducto>({
-    denominacion: "",
-    precioVenta: 0,
-    descripcion: "",
-    habilitado: true,
-    codigo: "",
-    idCategoria: 0,
-    idAlergenos: [],
-    imagenes: [],
-  });
+	const [formData, setFormData] = useState<ICreateProducto>({
+		denominacion: "",
+		precioVenta: 0,
+		descripcion: "",
+		habilitado: true,
+		codigo: "",
+		idCategoria: 0,
+		idAlergenos: [],
+		imagenes: [],
+	});
 
-  const handleChange = (e: React.ChangeEvent<HTMLElement>) => {
-    const { name, value } = e.target as HTMLInputElement | HTMLSelectElement;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+	const handleChange = (e: React.ChangeEvent<HTMLElement>) => {
+		const { name, value } = e.target as HTMLInputElement | HTMLSelectElement;
+		setFormData((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	};
 
-  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, checked } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: checked,
-    }));
-  };
+	const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, checked } = e.target;
+		setFormData((prev) => ({
+			...prev,
+			[name]: checked,
+		}));
+	};
 
-  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+	const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
 
-    // Validación para asegurarse que los campos estén completos
-    const isDataComplete = Object.values(formData).every(
-      (value) => value !== "" && value !== null && value !== undefined
-    );
+		// Validación para asegurarse que los campos estén completos
+		const isDataComplete = Object.values(formData).every(
+			(value) => value !== "" && value !== null && value !== undefined
+		);
 
-    if (!isDataComplete) {
-      Swal.fire({
-        icon: "warning",
-        title: "Rellene todos los campos",
-        showCancelButton: true,
-      });
-      return;
-    }
+		if (!isDataComplete) {
+			Swal.fire({
+				icon: "warning",
+				title: "Rellene todos los campos",
+				showCancelButton: true,
+			});
+			return;
+		}
 
-    try {
-      await productoService.createProduct(formData)
-      Swal.fire({
-        icon: "success",
-        title: "Producto agregado correctamente",
-        showCancelButton: false,
-        timer: 500,
-      });
-      onHide();
-      window.location.reload();
-    } catch (e) {
-      console.error(e);
-    }
-  };
+		try {
+			await productoService.createProduct(formData);
+			Swal.fire({
+				icon: "success",
+				title: "Producto agregado correctamente",
+				showCancelButton: false,
+				timer: 500,
+			});
+			onHide();
+			window.location.reload();
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
-  return (
-    <Modal show={show} onHide={onHide} backdrop="static" centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Crear Producto</Modal.Title>
-      </Modal.Header>
+	return (
+		<Modal
+			show={show}
+			onHide={onHide}
+			backdrop="static"
+			centered
+		>
+			<Modal.Header closeButton>
+				<Modal.Title>Crear Producto</Modal.Title>
+			</Modal.Header>
 
-      <Modal.Body>
-        <Form>
-          <Form.Group controlId="denominacion">
-            <Form.Label>Denominación</Form.Label>
-            <Form.Control
-              type="text"
-              name="denominacion"
-              value={formData.denominacion}
-              onChange={handleChange}
-              required
-            />
-          </Form.Group>
+			<Modal.Body>
+				<Form>
+					<Form.Group controlId="denominacion">
+						<Form.Label>Denominación</Form.Label>
+						<Form.Control
+							type="text"
+							name="denominacion"
+							value={formData.denominacion}
+							onChange={handleChange}
+							required
+						/>
+					</Form.Group>
 
-          <Form.Group controlId="precioVenta">
-            <Form.Label>Precio de Venta</Form.Label>
-            <Form.Control
-              type="number"
-              name="precioVenta"
-              value={formData.precioVenta}
-              onChange={handleChange}
-              required
-            />
-          </Form.Group>
+					<Form.Group controlId="precioVenta">
+						<Form.Label>Precio de Venta</Form.Label>
+						<Form.Control
+							type="number"
+							name="precioVenta"
+							value={formData.precioVenta}
+							onChange={handleChange}
+							required
+						/>
+					</Form.Group>
 
-          <Form.Group controlId="descripcion">
-            <Form.Label>Descripción</Form.Label>
-            <Form.Control
-              type="text"
-              name="descripcion"
-              value={formData.descripcion}
-              onChange={handleChange}
-              required
-            />
-          </Form.Group>
+					<Form.Group controlId="descripcion">
+						<Form.Label>Descripción</Form.Label>
+						<Form.Control
+							type="text"
+							name="descripcion"
+							value={formData.descripcion}
+							onChange={handleChange}
+							required
+						/>
+					</Form.Group>
 
-          <Form.Group controlId="codigo">
-            <Form.Label>Código</Form.Label>
-            <Form.Control
-              type="text"
-              name="codigo"
-              value={formData.codigo}
-              onChange={handleChange}
-              required
-            />
-          </Form.Group>
+					<Form.Group controlId="codigo">
+						<Form.Label>Código</Form.Label>
+						<Form.Control
+							type="text"
+							name="codigo"
+							value={formData.codigo}
+							onChange={handleChange}
+							required
+						/>
+					</Form.Group>
 
-          <Form.Group controlId="habilitado">
-            <Form.Check
-              type="checkbox"
-              name="habilitado"
-              label="Habilitado"
-              checked={formData.habilitado}
-              onChange={handleCheckboxChange}
-            />
-          </Form.Group>
+					<Form.Group controlId="habilitado">
+						<Form.Check
+							type="checkbox"
+							name="habilitado"
+							label="Habilitado"
+							checked={formData.habilitado}
+							onChange={handleCheckboxChange}
+						/>
+					</Form.Group>
 
-          <Form.Group controlId="idCategoria">
-            <Form.Label>Categoría</Form.Label>
-            <Form.Control
-              as="select"
-              name="idCategoria"
-              value={formData.idCategoria}
-              onChange={handleChange}
-              required
-            >
-              <option value={0}>Seleccionar categoría</option>
-              {/* Aquí se deberían cargar las categorías disponibles */}
-            </Form.Control>
-          </Form.Group>
-        </Form>
-      </Modal.Body>
+					<Form.Group controlId="idCategoria">
+						<Form.Label>Categoría</Form.Label>
+						<Form.Control
+							as="select"
+							name="idCategoria"
+							value={formData.idCategoria}
+							onChange={handleChange}
+							required
+						>
+							<option value={0}>Seleccionar categoría</option>
+							{/* Aquí se deberían cargar las categorías disponibles */}
+						</Form.Control>
+					</Form.Group>
+				</Form>
+			</Modal.Body>
 
-      <Modal.Footer>
-        <Button variant="secondary" onClick={onHide}>
-          Cancelar
-        </Button>
-        <Button variant="primary" onClick={handleSubmit}>
-          Guardar
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+			<Modal.Footer>
+				<Button
+					variant="secondary"
+					onClick={onHide}
+				>
+					Cancelar
+				</Button>
+				<Button
+					variant="primary"
+					onClick={handleSubmit}
+				>
+					Guardar
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
 };
